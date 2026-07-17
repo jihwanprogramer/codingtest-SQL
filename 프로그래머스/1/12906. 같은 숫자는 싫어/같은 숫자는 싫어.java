f0d9f985851dec7ecc550cff17e22d6d8a6a3632
KIM JI HWAN <130281249+jihwanprogramer@users.noreply.github.com>
2026-07-18 06:33:46 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 25.24 ms, Memory: 118 MB -BaekjoonHub

---
+++ 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+public class Solution {
+    public int[] solution(int[] arr) {
+
+        Deque<Integer> stack = new ArrayDeque<>();
+
+        for(int i=0; i<arr.length; i++){
+            if(stack.isEmpty() || stack.peek() != arr[i]){
+                stack.push(arr[i]);
+            }
+        }
+
+        int[] answer = new int[stack.size()];
+
+        for(int i=answer.length-1; i>=0; i--){
+            answer[i] = stack.pop();
+        }
+
+        return answer;
+    }
+}
