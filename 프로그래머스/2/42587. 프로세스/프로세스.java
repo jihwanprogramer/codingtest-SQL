e4c0cd2311f0e8e507256b5650910298915937c4
KIM JI HWAN <130281249+jihwanprogramer@users.noreply.github.com>
2026-07-23 01:57:16 +0900

    [level 2] Title: 프로세스, Time: 2.75 ms, Memory: 87.2 MB -BaekjoonHub

---
+++ 프로그래머스/2/42587. 프로세스/프로세스.java
@@ -0,0 +1,39 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] priorities, int location) {
+        Queue<int[]> queue = new LinkedList<>();
+
+        for (int i = 0; i < priorities.length; i++) {
+            queue.offer(new int[]{i, priorities[i]});
+        }
+
+        int answer = 0;
+
+        while (!queue.isEmpty()) {
+
+            int[] cur = queue.poll();
+
+            boolean higher = false;
+
+            for (int[] q : queue) {
+                if (q[1] > cur[1]) {
+                    higher = true;
+                    break;
+                }
+            }
+
+            if (higher) {
+                queue.offer(cur);
+            } else {
+                answer++;
+
+                if (cur[0] == location) {
+                    return answer;
+                }
+            }
+        }
+
+        return answer;
+    }
+}
