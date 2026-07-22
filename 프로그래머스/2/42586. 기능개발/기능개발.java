a11dfac1745d797f4606aa74535b90a2043e78f9
KIM JI HWAN <130281249+jihwanprogramer@users.noreply.github.com>
2026-07-23 00:37:49 +0900

    [level 2] Title: 기능개발, Time: 3.53 ms, Memory: 88.8 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.java
+++ 프로그래머스/2/42586. 기능개발/기능개발.java
@@ -2,32 +2,16 @@ import java.util.*;
 
 class Solution {
     public int[] solution(int[] progresses, int[] speeds) {
-
-        List<Integer> result = new ArrayList<>();
-
-        // 첫 번째 기능의 완료일
-        int standardDay = (100 - progresses[0] + speeds[0] - 1) / speeds[0];
-        int count = 1;
-
-        for (int i = 1; i < progresses.length; i++) {
-
-            // 현재 기능 완료일
-            int day = (100 - progresses[i] + speeds[i] - 1) / speeds[i];
-
-            if (day <= standardDay) {
-                // 앞 기능보다 먼저 끝나므로 같이 배포
-                count++;
-            } else {
-                // 새로운 배포 시작
-                result.add(count);
-                standardDay = day;
-                count = 1;
+        int day = -1;
+        int[] end_day= new int[100];
+        
+        for(int i=0;i<progresses.length;i++){
+            while(progresses[i]+ day*speeds[i] <100){
+                day++;
             }
+            end_day[day]++;
         }
-
-        // 마지막 배포 추가
-        result.add(count);
-
-        return result.stream().mapToInt(i -> i).toArray();
+        
+        return Arrays.stream(end_day).filter(i->i!=0).toArray();
     }
 }
