dd03c9ae4ba5a19b75d1478c61019e747ba73577
KIM JI HWAN <130281249+jihwanprogramer@users.noreply.github.com>
2026-08-10 12:23:22 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 3.58 ms, Memory: 57.9 MB -BaekjoonHub

---
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
@@ -0,0 +1,52 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[][] maps) {
+        int n = maps.length;
+        int m = maps[0].length;
+        
+        boolean[][] check = new boolean[n][m];
+        return bfs(0,0,maps,check);
+    }
+    
+    public int bfs(int x,int y,int[][] maps,boolean[][] check){
+        int n = maps.length;
+        int m = maps[0].length;
+        
+        int answer =0;
+        int[] dc = {1,0,-1,0};
+        int[] dr = {0,1,0,-1};
+        Queue<int[]> queue = new ArrayDeque<>();
+        queue.add(new int[]{x,y,1});
+        check[x][y] = true;
+        
+        while(!queue.isEmpty()){
+            int[] cur = queue.poll();
+            int a = cur[0];
+            int b = cur[1];
+            int time = cur[2];
+        
+            for(int i=0;i<4;i++){
+                int nx = a + dc[i];
+                int ny = b + dr[i]; 
+                if(nx>=0 && nx<n && ny>=0 && ny<m){
+                    if(maps[nx][ny] == 1 && !check[nx][ny]){
+                        if(nx == n-1 && ny ==m -1){
+                        return time+1;
+                        }
+                        
+                        check[nx][ny] = true;
+                        queue.add(new int[]{nx,ny,time+1});
+                    }
+                    
+                    
+                    
+                }
+                
+            }
+            
+        }
+        
+        return -1;
+    }
+}
