c33f3858e2857e3cc9bb2168bccfec15cdb2b5cd
KIM JI HWAN <130281249+jihwanprogramer@users.noreply.github.com>
2026-08-22 20:10:19 +0900

    [level 3] Title: 네트워크, Time: 0.18 ms, Memory: 74.5 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.java
+++ 프로그래머스/3/43162. 네트워크/네트워크.java
@@ -1,22 +1,24 @@
 class Solution {
-    public int answer = 0 ;
+    public int answer = 0;
     public int solution(int n, int[][] computers) {
-        boolean[] visited = new boolean[n];
+        int answer = 0;
+        
+        
+        boolean[]visited = new boolean[n];
         
         for(int i=0;i<n;i++){
-            if(!visited[i]){
-                answer++;
-                dfs(n,computers,i,visited);
-            }
+           if(!visited[i]){
+               answer++;
+               dfs(n,computers,i,visited);
+           }
         }
-        
         return answer;
     }
-     
-    void dfs(int n,int[][] computers,int curr,boolean[] visited){
-        visited[curr] = true; 
-        for(int i = 0; i < n ;i++){
-            if(computers[curr][i] == 1 && !visited[i]){            
+    
+    void dfs(int n, int[][] computers,int curr,boolean[] visited){
+        visited[curr] = true;
+        for(int i=0;i<n;i++){
+            if(computers[curr][i] == 1 && !visited[i]){
                 dfs(n,computers,i,visited);
             }
         }
